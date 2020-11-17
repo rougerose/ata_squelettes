318830f9f2f07d2768df352a10f3a39f74ee8ef6
christophe le drean <christophe@rouge-rose.net>
2020-11-17 17:21:14 +0100

    Options pour Zcore

---
+++ ata_squelettes_options.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+  return;
+}
+
+// Zcore
+if (!isset($GLOBALS['z_blocs'])) {
+	$GLOBALS['z_blocs'] = array(
+		'content',
+		'head_js',
+		'head',
+		'header',
+		'footer',
+	);
+}
