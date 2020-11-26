02c0351224cbfa8b1d3f196b7f82c84c5396e929
christophe le drean <christophe@rouge-rose.net>
2020-11-26 10:05:39 +0100

    Suite développement

--- ata_squelettes_pipelines.php
+++ ata_squelettes_pipelines.php
@@ -3,17 +3,11 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
   return;
 }
-/*
+
 function ata_squelettes_recuperer_fond($flux) {
-	if ($flux['args']['fond'] == 'javascript/gis.js') {
-		// leaflet-sidebar
-		$js = spip_file_get_contents(find_in_path('dist/js/L.Control.Sidebar.js'));
-		// leaflet-easybutton
-		$js .= "\n".spip_file_get_contents(find_in_path('dist/js/easy-button.js'));
-		// leaflet-spin
-		$js .= "\n".spip_file_get_contents(find_in_path('dist/js/leaflet.spin.min.js'));
-		$flux['data']['texte'] .= "\n".$js;
-	}
+	// if ($flux['args']['fond'] == 'javascript/gis.js') {
+	// 	$js = spip_file_get_contents(find_in_path('dist/js/vendor/L.Control.Sidebar.js'));
+	// 	$flux['data']['texte'] .= "\n" . $js;
+	// }
 	return $flux;
 }
-*/
