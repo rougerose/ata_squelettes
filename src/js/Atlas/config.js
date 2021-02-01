d4d6888d6067ba47a05d72e0266a040615127377
christophe le drean <christophe@rouge-rose.net>
2021-02-01 14:08:18 +0100

    JS config : nettoyage valeurs inutiles

--- src/js/Atlas/config.js
+++ src/js/Atlas/config.js
@@ -9,12 +9,10 @@ export const config = {
         xxl: 84,
     },
     searchBox: {
-        containerId: "SearchBox",
         btnAdvancedId: "#openAdvancedSearch",
         panelClassName: "mp-SearchBox_Panel",
     },
     keywords: {
-        containerClassName: "mp-Keywords-toSelect",
         ulClassName: "mp-Keywords_List",
         liClassName: {
             main: "mp-Keywords_Item",
@@ -22,14 +20,6 @@ export const config = {
         },
         labelClassName: "mp-Keywords_Label",
         btnDeleteClassName: "mp-Keywords_BtnDelete",
-        tooltipClassName: "mp-Keywords_BtnTooltip",
-        activities: {
-            liClassName: "mp-Keywords_Item-toSelect",
-            aClassName: "mp-Keywords_Label-activity",
-        },
-    },
-    queryKeywords: {
-        containerId: "js-Keywords",
     },
     modal: {
         setTabIndex: true,
@@ -37,16 +27,5 @@ export const config = {
         closedClass: "is-closed",
         bottomBarClass: "mp-Modal_Bar",
         containerId: "Modal",
-        btnClassName: "mp-Modal_BtnAction",
-        headerClassName: "mp-Modal_HeaderContent",
-        bodyClassName: "mp-Modal_Body",
-        profile: {
-            headerClassName: "mp-OrgProfile_Header",
-            badge: {
-                imgClassName: "mp-OrgProfile_Logo",
-                figClassName: "mp-OrgProfile_Badge",
-            },
-            nameClassName: "mp-OrgProfile_Name",
-        },
     },
 };
