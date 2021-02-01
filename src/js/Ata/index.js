d753b5812467f61b77a70d9583d7aa9a11c30e6b
christophe le drean <christophe@rouge-rose.net>
2021-02-01 12:18:56 +0100

    Js nettoyage

--- src/js/Ata/index.js
+++ src/js/Ata/index.js
@@ -1,18 +1,8 @@
 import { navInit } from "./components/nav";
 import { pageInit } from "./components/page";
-// import { mapInit } from "./map/index";
-// import { addKeyword } from "./map/keywords";
-
 
 // Initialiser les scripts relatifs au site
 export const init = () => {
 	navInit();
 	pageInit();
 }
-
-// Initialiser le module relatif à l'atlas
-// export const map = (mapObj) => {
-// 	mapInit(mapObj);
-// }
-
-// export { addKeyword };
--- src/js/Ata/map/markers.js
+++
@@ -1,6 +0,0 @@
-const handleClickOnMarker = (el) => {
-	console.log(el);
-	// prévoir de retirer et ajouter le gestionnaire
-}
-
-export { handleClickOnMarker };
