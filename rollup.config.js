5c06de4d6f0c5841d3bf422b115a2785e6846f8c
christophe le drean <christophe@rouge-rose.net>
2024-01-11 17:20:03 +0100

    build: compilation js et css

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+
+const srcDir = "src/js/";
+const destDir = "dist/js/";
+const terserOptions = {
+	compress: { passes: 3 },
+	module: true,
+	mangle: true,
+	nameCache: {},
+};
+const outputPlugins = () => [
+	process.env.NODE_ENV === "production" && terser(terserOptions),
+];
+
+function build(src, dist, name, format) {
+	return {
+		input: srcDir + src,
+		plugins: [resolve(), commonjs()],
+		output: {
+			file: destDir + dist,
+			format: format,
+			name: name,
+			plugins: outputPlugins(),
+		},
+	};
+}
+
+export default [
+	build("Ata/index.js", "ata.js", "Ata", "iife"),
+	build("Ata/ata_init.js", "ata_init.js", "", "es"),
+	build("Atlas/index.js", "atlas.js", "atlas", "iife"),
+	build(
+		"Atlas/atlas_autocomplete_widget.js",
+		"atlas_autocomplete_widget.js",
+		"",
+		"es"
+	),
+];
